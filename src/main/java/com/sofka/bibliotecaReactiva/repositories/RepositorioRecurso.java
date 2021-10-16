e2b0b0fc20d077ca54b18650262764a7e2b6ba29
Leonardo <leocv0205@gmail.com>
2021-10-16 12:00:38 -0500

    Se crea la clase ConfiguracionWebFlux

--- src/main/java/com/sofka/bibliotecaReactiva/repositories/RepositorioRecurso.java
+++ src/main/java/com/sofka/bibliotecaReactiva/repositories/RepositorioRecurso.java
@@ -6,6 +6,6 @@ import reactor.core.publisher.Flux;
 
 public interface RepositorioRecurso extends ReactiveCrudRepository<Recurso, String> {
     Flux<Recurso> findAllBytipoRecurso(final String tipoRecurso);
-    Flux<Recurso> findAllBytematicaRecurso(final String tematicaRecurso)
+    Flux<Recurso> findAllBytematicaRecurso(final String tematicaRecurso);
     Flux<Recurso> findAllBytipoYTematica(final String tipoRecurso, final String tematicaRecurso);
 }
