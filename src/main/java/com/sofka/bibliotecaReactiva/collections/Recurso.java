7c8eb2454b10758a66468b671bdc155ec9a70503
Leonardo <leocv0205@gmail.com>
2021-10-16 10:35:25 -0500

    Se crea la clase Recursos en el paquete collections

---
+++ src/main/java/com/sofka/bibliotecaReactiva/collections/Recurso.java
@@ -0,0 +1,27 @@
+package com.sofka.bibliotecaReactiva.collections;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document
+public class Recurso {
+    @Id
+    private String id;
+    private String nombreRecurso;
+    private String tematicaRecurso;
+    private String tipoRecurso;
+    private LocalDate fechaPrestamo;
+    private Integer cantidadDisponible;
+    private Integer cantidadPrestada;
+}
